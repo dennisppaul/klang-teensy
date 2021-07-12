30cb5a034a9c4c5850ae033694c349589aae0749
dennisppaul <dennis.paul@hfk-bremen.de>
2021-07-12 10:55:54 +0200

    cleaned up build script

--- build.sh
+++ build.sh
@@ -5,11 +5,26 @@ KLST_ARDUINO_SRC_PATH=../klangstrom/klangstrom/platforms/klangstrom-arduino/libr
 M_SRC_PATH=src
 M_TEMPLATE_PATH=template
 
+echo 
+echo "###"
+echo "### cleaning up source folder"
+
 if [ -d "$M_SRC_PATH" ]; then
 	rm -rf "$M_SRC_PATH"
 fi
 mkdir -p "$M_SRC_PATH"
 
+echo "### copying source files from klangstrom project"
+
 cp -r "$KLST_ARDUINO_SRC_PATH" "$M_SRC_PATH"
-cp -r "$M_TEMPLATE_PATH" "$M_SRC_PATH"
+
+echo "### cleaning up source files"
+
 rm "$M_SRC_PATH"/Klangstrom.h
+
+echo "### copying template files"
+
+cp -r "$M_TEMPLATE_PATH"/ "$M_SRC_PATH"
+
+echo "###"
+echo
